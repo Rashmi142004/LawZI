a89758478a9d1936fe97775ee6488aca931fd075
Rashmi142004 <rizz35436@gmail.com>
2025-07-01 19:18:10 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}"
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        lawzi: "#7B3F00", // Your brown color
-        background: "#1E1E1E" // Optional dark background
-      },
-      fontFamily: {
-        heading: ["'Poppins'", "sans-serif"]
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
